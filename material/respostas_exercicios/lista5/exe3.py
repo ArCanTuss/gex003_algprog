9d29e7cf4fa3e9d7c0a940ec82dd94bc2e4e7c5e
Daniel Di Domenico <ddomenico@inf.ufsm.br>
2018-04-02 11:33:25 -0300

    Correção lista5

--- material/respostas_exercicios/lista5/exe3.py
+++ material/respostas_exercicios/lista5/exe3.py
@@ -4,10 +4,11 @@ n2 = int(input("Informe um número N2: "))
 quoc = 0
 resto = 0
 while n1 > 0 and resto == 0:
-	n1 = n1 - n2
-	quoc += 1
-	
 	if n2 > n1:
 		resto = n1
+	else:
+		n1 = n1 - n2
+		quoc += 1
+	
 
 print("Quociente: {0} | Resto: {1}".format(quoc, resto))
