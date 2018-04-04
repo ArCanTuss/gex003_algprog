e4da87271fe1b6b35b21017224832e787033a174
Daniel Di Domenico <ddomenico@inf.ufsm.br>
2018-04-04 14:11:57 -0300

    Lista 5 exe 3 - nova versão

--- material/respostas_exercicios/lista5/exe3.py
+++ material/respostas_exercicios/lista5/exe3.py
@@ -1,6 +1,7 @@
 n1 = int(input("Informe um número N1: "))
 n2 = int(input("Informe um número N2: "))
 
+'''SOLUÇÃO 1
 quoc = 0
 resto = 0
 while n1 > 0 and resto == 0:
@@ -12,3 +13,13 @@ while n1 > 0 and resto == 0:
 	
 
 print("Quociente: {0} | Resto: {1}".format(quoc, resto))
+'''
+
+#SOLUÇÃO 2
+quoc = 0
+while n1 >= n2:
+	n1 = n1 - n2
+	quoc += 1
+	
+
+print("Quociente: {0} | Resto: {1}".format(quoc, n1))
