97644b553937f6dd78e9ca671208b4e880856372
Daniel Di Domenico <danidomenico.chapeco@gmail.com>
2018-06-06 19:53:01 -0300

    Respostas exercícios lista 12

--- material/respostas_exercicios/lista12/exe1.py
+++ material/respostas_exercicios/lista12/exe1.py
@@ -3,24 +3,27 @@ class Data: #Estrutura Data
 	mes = 0
 	dia = 0
 
-maiorData = Data()
-
-for i in range(5):
+def ler_data():
 	data = Data()
 	print()
 	data.ano = int(input("Informe o ano: "))
 	data.mes = int(input("Informe o mês: "))
 	data.dia = int(input("Informe o dia: "))
+	return data
 	
-	maiorAno = data.ano > maiorData.ano
-	maiorMes = data.ano == maiorData.ano and data.mes > maiorData.mes
-	maiorDia = data.ano == maiorData.ano and data.mes == maiorData.mes and data.dia > maiorData.dia
-	print(maiorAno, maiorMes, maiorDia)
+
+menorData = ler_data()
+
+for i in range(5):
+	data = ler_data()
 	
-	if maiorAno or maiorMes or maiorDia:
-		print("Entrou")
-		maiorData = data
+	menorAno = data.ano < menorData.ano
+	menorMes = data.ano == menorData.ano and data.mes < menorData.mes
+	menorDia = data.ano == menorData.ano and data.mes == menorData.mes and data.dia < menorData.dia
+	
+	if menorAno or menorMes or menorDia:
+		menorData = data
 
 print()
-print("Maior data:")
-print("{}/{}/{}".format(maiorData.dia, maiorData.mes, maiorData.ano))
+print("Menor data:")
+print("{}/{}/{}".format(menorData.dia, menorData.mes, menorData.ano))
